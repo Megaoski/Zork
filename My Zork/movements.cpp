b0e92dcbd745a5ecf9c858b4ef990831be7e0699
Megaoski <oscar_2596@hotmail.com>
2016-03-11 00:07:16 +0100

    Moves source file+ header
    Introducing first code of the movement ingame

---
+++ My Zork/movements.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include"movements.h"
+
+
+using namespace std;
+
+
+
+/*/class room
+{
+ public:
+	char rooms[40];
+	char *r = new char[40];
+	char *r = &rooms[40];
+	char *r = { "campfire;dark forest;haunted castle" };
+};/*/
+
---
+++ My Zork/movements.h
@@ -0,0 +1,10 @@
+#ifndef movements_h
+#define movements_h
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+#endif
